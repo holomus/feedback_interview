9cc103e07067fc4745445eec49fb42d73680313b
Sanjar Akhmedov <leoabrahaam@gmail.com>
2020-07-17 20:21:35 +0500

    Interview test project

--- Feedback_project/urls.py
+++ Feedback_project/urls.py
@@ -21,6 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('feedback/', include('feedback.urls')),
-    path('', include('main.urls')),
+    path('', include('feedback.urls')),
+    path('home/', include('main.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- feedback/urls.py
+++ feedback/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from feedback import views
 
 urlpatterns = [
-    path('create', views.create, name="create"),
-    path('success', views.success, name='success')
+    path('feedback/create', views.create, name="create"),
+    path('feedback/success', views.success, name='success'),
+    path('', views.create)
 ]
